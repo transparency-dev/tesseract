87d00981f572a802ac78e4e02de9702a20e60a27
Philippe Boneff <phb@google.com>
2025-06-13 19:21:47 +0000

    [preloader] more preloader load

--- deployment/modules/gcp/vm/main.tf
+++ deployment/modules/gcp/vm/main.tf
@@ -199,11 +199,11 @@ module "preloader-container" {
     image = "us-central1-docker.pkg.dev/static-ct-staging/docker-staging/preloader@sha256:4fd99df0ba68b726cef52d41c05a2e58dbd077ee4eddd7396e871a91caa46394"
     args = [
       "--target_log_uri=http://${module.gce-ilb.ip_address}:6962/${var.base_name}${var.origin_suffix}",
-      "--source_log_uri=https://ct.googleapis.com/logs/us1/argon2025h1",
-      "--start_index=300000",
-      "--num_workers=20", 
+      "--source_log_uri=https://ct.googleapis.com/logs/us1/argon2025h1", // TODO(phbnf): make this a flag
+      "--start_index=774077265", // TODO(phbnf): make this a flag
+      "--num_workers=400", 
       "--parallel_fetch=20", 
-      "--parallel_submit=20",
+      "--parallel_submit=400",
     ]
     tty : true # maybe remove this
   }
