4ee6a64ec5a44cb0be843ec07da69bd3af647bca
Roger Ng <rogerng@google.com>
2025-07-10 17:45:44 +0000

    Install OpenTofu in alpine/terragrunt container

--- deployment/modules/gcp/cloudbuild/conformance/main.tf
+++ deployment/modules/gcp/cloudbuild/conformance/main.tf
@@ -64,13 +64,10 @@ resource "google_cloudbuild_trigger" "build_trigger" {
       name   = "alpine/terragrunt"
       script = <<EOT
         # Download the installer script:
-        curl --proto '=https' --tlsv1.3 -fsSL https://get.opentofu.org/install-opentofu.sh -o install-opentofu.sh
-        # Alternatively: wget --secure-protocol=TLSv1_2 --https-only https://get.opentofu.org/install-opentofu.sh -O install-opentofu.sh
+        wget https://get.opentofu.org/install-opentofu.sh -O install-opentofu.sh
 
         # Give it execution permissions:
         chmod +x install-opentofu.sh
-
-        # Please inspect the downloaded script
 
         # Run the installer:
         ./install-opentofu.sh --install-method apk
