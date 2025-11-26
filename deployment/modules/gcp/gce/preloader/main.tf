a2a9217320d716078bdc2f90f7831c4adccc4b1a
Al Cutter <al@google.com>
2025-11-26 16:02:16 +0000

    Migrate preloader to cloud-init

--- deployment/modules/gcp/gce/preloader/main.tf
+++ deployment/modules/gcp/gce/preloader/main.tf
@@ -10,29 +10,62 @@ terraform {
 
 locals {
   preloader_service_account_id = var.env == "" ? "preloader-sa" : "preloader-${var.env}-sa"
+
+  preloader_container_name = "preloader-${var.env}"
+
+  # docker_run_args are provided to the docker run command.
+  # Use this to configure docker-specific things.
+  docker_run_args = join(" ", compact([
+    # Ensure that TesseraCT logs are delivered to GCP logging.
+    "--log-driver=gcplogs",
+  ]))
+
+  # Args for the preloader command.
+  preloader_args = join(" ", [
+    "--target_log_uri=${var.target_log_uri}",
+    "--source_log_uri=${var.source_log_uri}",
+    "--start_index=${var.start_index}",
+    "--continuous=true",
+    "--num_workers=500",
+    "--parallel_fetch=20",
+    "--parallel_submit=1500",
+    "--v=2",
+  ])
+
+  preloader_cloud_init = <<EOT
+    #cloud-config
+
+    users:
+      - name: preloader
+        uid: 2000
+        groups: docker # Add the user to the Docker group
+
+    write_files:
+      - path: /etc/systemd/system/preloader.service
+        permissions: 0644
+        owner: root
+        content: |
+          [Unit]
+          Description=Run Preloader
+
+          [Service]
+          ExecStartPre=sudo -u preloader /usr/bin/docker-credential-gcr configure-docker --registries ${var.location}-docker.pkg.dev
+          ExecStart=sudo -u preloader -E /usr/bin/docker run --rm -u 2000 --name=${local.preloader_container_name} ${local.docker_run_args} ${var.server_docker_image} ${local.preloader_args}
+          ExecStop=sudo -u preloader /usr/bin/docker stop ${local.preloader_container_name}
+          ExecStopPost=sudo -u /usr/bin/docker rm ${local.preloader_container_name}
+          StandardOutput=journal
+          StandardError=journal
+
+    runcmd:
+      - systemctl daemon-reload
+      - systemctl start preloader.service
+    EOT
+
 }
 
-module "gce_container_preloader" {
-  # https://github.com/terraform-google-modules/terraform-google-container-vm
-  source  = "terraform-google-modules/container-vm/google"
-  version = "~> 2.0"
-
-  container = {
-    image = var.server_docker_image,
-    args = [
-      "--target_log_uri=${var.target_log_uri}",
-      "--source_log_uri=${var.source_log_uri}",
-      "--start_index=${var.start_index}",
-      "--continuous=true",
-      "--num_workers=500",
-      "--parallel_fetch=20",
-      "--parallel_submit=1500",
-      "--v=2",
-    ]
-    tty : true # maybe remove this
-  }
-
-  restart_policy = "Always"
+data "google_compute_image" "cos" {
+  family  = "cos-121-lts"
+  project = "cos-cloud"
 }
 
 resource "google_compute_instance" "preloader" {
@@ -42,22 +75,18 @@ resource "google_compute_instance" "preloader" {
 
   tags = ["preloader"]
 
-  boot_disk {
-    initialize_params {
-      image = module.gce_container_preloader.source_image
-      labels = {
-        my_label = "value"
-      }
-    }
-  }
-
   network_interface {
     network = "default"
   }
 
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.cos.self_link
+    }
+  }
+
   labels = {
     environment  = var.env
-    container-vm = module.gce_container_preloader.vm_container_label
   }
 
   # Come back to this: the start_index needs to be manually edited
@@ -69,9 +98,9 @@ resource "google_compute_instance" "preloader" {
   allow_stopping_for_update = true
 
   metadata = {
-    gce-container-declaration = module.gce_container_preloader.metadata_value
     google-logging-enabled    = "true"
     google-monitoring-enabled = "true"
+    user-data                 = local.preloader_cloud_init
   }
 
   service_account {
