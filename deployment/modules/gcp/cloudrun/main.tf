debc9397af44aac6e42c177478a29fc1e8f59800
Philippe Boneff <phb@google.com>
2026-03-09 17:59:57 +0000

    Enable grpc GCS client for conformance test (#765)
    * enable grpc for conformance test
    
    * comments

--- deployment/modules/gcp/cloudrun/main.tf
+++ deployment/modules/gcp/cloudrun/main.tf
@@ -60,6 +60,7 @@ resource "google_cloud_run_v2_service" "default" {
         "--batch_max_age=${var.batch_max_age}",
         "--roots_remote_fetch_url=${var.roots_remote_fetch_url}",
         "--roots_remote_fetch_interval=${var.roots_remote_fetch_interval}",
+        "--gcs_use_grpc=true",
       ])
       ports {
         container_port = 6962
