64e0624d50dcfab48051e674a2707516596e350f
Philippe Boneff <phb@google.com>
2026-04-15 17:03:54 +0100

    wire create_internal_load_balancer (#830)

--- deployment/modules/gcp/tesseract/gce/outputs.tf
+++ deployment/modules/gcp/tesseract/gce/outputs.tf
@@ -1,6 +1,6 @@
 output "tesseract_url" {
   description = "The submission URL of the running TesseraCT server"
-  value       = module.ilb.tesseract_url
+  value       = var.create_internal_load_balancer ? module.ilb[0].tesseract_url : "No private URL configured."
 }
 
 output "tesseract_bucket_name" {
